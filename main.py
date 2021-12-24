ed05b7de4e2e4686a8ee766f3e7d80200ebabd6b
LongNguyen <nqlong0709@gmail.com>
2021-12-24 15:24:01 +0700

    update requirement

--- main.py
+++ main.py
@@ -1,31 +1,39 @@
+import win32api
+import win32con
 import time
 import threading
 from pynput.mouse import Button, Controller
 from pynput.keyboard import Listener, KeyCode
 import logging
 
-logger = logging.info
 logger = print
 delay = 5
 button = Button.left
-start_stop_key = KeyCode(char='a')
-exit_key = KeyCode(char='e')
+start_stop_key = KeyCode(char=']')
+exit_key = KeyCode(char='[')
+
+
+def click():
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0)
+    time.sleep(0.5)
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0)
+
 
 
 class ClickMouse(threading.Thread):
-    def __init__(self, delay, button):
+    def __init__(self, delay, interval=1):
         super().__init__()
         self.delay = delay
-        self.button = button
+        self.interval = interval
         self.running = False
         self.program_running = True
 
     def start_clicking(self):
-        logger('start clicking')
+        logger('start fishing')
         self.running = True
 
     def stop_clicking(self):
-        logger('stop clicking')
+        logger('stop fishing')
         self.running = False
 
     def exit(self):
@@ -35,19 +43,22 @@ class ClickMouse(threading.Thread):
     def run(self):
         while self.program_running:
             while self.running:
-                logger('click')
-                mouse.click(self.button)
+                logger('throw rod')
+                click()
                 time.sleep(self.delay)
+                logger('catch')
+                click()
+                time.sleep(self.interval)
 
-mouse = Controller()
-click_thread = ClickMouse(delay=delay, button=button)
+
+
+click_thread = ClickMouse(delay=4, interval=0.5)
 click_thread.start()
 
 
 def on_press(key):
     logger(key)
     if key == start_stop_key:
-        logger('hello')
         if click_thread.running:
             click_thread.stop_clicking()
         else:
